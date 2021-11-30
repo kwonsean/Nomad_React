2b62c7454be2e878b0add1c1a096f6ced366adf3
kwonsean <gownn12@naver.com>
2021-11-30 16:55:24 +0900

    feat: Todo 완료

--- react-for-beginners/src/index.js
+++ react-for-beginners/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import Todo from './Todo'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Todo />
   </React.StrictMode>,
   document.getElementById('root')
 )
